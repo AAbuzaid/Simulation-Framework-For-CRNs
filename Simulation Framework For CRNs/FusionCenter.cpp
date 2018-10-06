e6bd15ac365619aa418d9801f242aee3e76a06fa
AhmadMustafa015 <ahm0157454@fet.ju.edu.jo>
2018-10-06 16:52:37 +0300

    updating

--- Simulation Framework For CRNs/FusionCenter.cpp
+++ Simulation Framework For CRNs/FusionCenter.cpp
@@ -57,7 +57,7 @@ void FusionCenter::getEmptyBands(const std::vector<int> &Bands)
 	emptyBands[SuId] = Bands;
 
 }
-void FusionCenter::bandsOccupiedBySU(const std::vector<int> &suBand)
+void FusionCenter::bandsOccupantedBySU(const std::vector<int> &suBand)
 {
 	/*for (int col = 0; col < maxSUsband; col++)
 	{
